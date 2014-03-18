e69d840d703b54f83c804a03d3c65d0a49bc82b7
Vincent Risack <risackvincent@hotmail.com>
2014-03-18 09:59:13 +0100

    added class with constuctor

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
-
+/**
+* 
+*/
+class events
+{ public $image , $title , $description , $date , $time , $website , $email;
+	
+	function __construct($image , $title , $description , $date , $time , $website , $email)
+	{
+		$this->$image = $image;
+		$this->$title = $title;
+		$this->$description = $description;
+		$this->$date = $date;
+		$this->$time = $time;
+		$this->$website = $website;
+		$this->$email = $email;
+	}
+}
 	include_once("lib/html.php");
 
 	$user_name ="";
