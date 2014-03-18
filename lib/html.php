90e5fa9b772a3ad88e31c990049dcdfffa225c47
Vincent Risack <risackvincent@hotmail.com>
2014-03-18 09:44:44 +0100

    start of exercise

---
+++ lib/html.php
@@ -0,0 +1,73 @@
+<?php 
+
+class HtmlElement{
+	protected $tag = "p";
+	protected $attributes = "";
+
+	public function __construct($content, $attributes = array()){
+		$this->attributes = $attributes;
+		$this->content = $content;
+		$this->attributes = $this->calculateAttributes($attributes);
+	}
+
+	public function __toString(){
+		return "<{$this->tag}{$this->attributes}>$this->content</{$this->tag}>";
+	}
+
+	protected function calculateAttributes($attributes){
+		$result = "";
+		foreach ($attributes as $attribute => $value) {
+			$result .= isset($attributes[$attribute]) ? " {$attribute}=\"{$value}\"" : "";	
+		}
+		return $result;
+	}
+}
+
+class HtmlVoidElement extends HtmlElement{
+	public function __construct($attributes = array()){
+		parent::__construct("", $attributes);
+	}
+	public function __toString(){
+		return "<{$this->tag}{$this->attributes} />";
+	}
+}
+
+class Paragraph extends HtmlElement{
+}
+
+class Heading extends HtmlElement{
+	public function __construct($content, $level = 1, $attributes = array()){
+		$this->tag = "h$level";
+		parent::__construct($content, $attributes);
+	}
+}
+
+class Div extends HtmlElement{
+	public function __construct($content, $attributes = array()){
+		$this->tag = "div";
+		parent::__construct($content,$attributes);
+	}
+}
+
+class Button extends HtmlElement{
+	public function __construct($content, $attributes = array()){
+		$this->tag = "button";
+		parent::__construct($content,$attributes);
+	}
+}
+
+class Input extends HtmlVoidElement{
+	public function __construct($name, $type = "text", $attributes = array()){
+		$this->tag = "input";
+		$attributes["name"] = $name;
+		$attributes["type"] = $type;
+		parent::__construct($attributes);
+	}
+}
+
+class Form extends HtmlElement{
+	public function __construct($content, $attributes = array()){
+		$this->tag = "form";
+		parent::__construct($content,$attributes);
+	}
+}
