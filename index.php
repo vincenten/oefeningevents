50fcc4d7b2351292929c2208ab06cdf3e83cb2d8
Vincent Risack <risackvincent@hotmail.com>
2014-03-18 12:05:39 +0100

    added pages

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 	include_once("event.php");
 	include_once("lib/html.php");
 	$imageSize = ["height" => 50, "width" => 50];
-$events = [new Event ('jep.jpg' , 'event 1' , 'description' , 'date' , 'time' , 'website' ,'email'),
-new Event ('apple.jpg' , 'event 2' , 'description' , 'date' , 'time' , 'website' ,'email'),
-new Event ('linux.jpg' , 'event 3' , 'description' , 'date' , 'time' , 'website', 'email'),
-new Event ('microzach.jpg' , 'event 4' , 'description' , 'date' , 'time' , 'website' ,'email'),
-new Event ('twitter1.gif' , 'event 5' , 'description' , 'date' , 'time' , 'website' ,'email')];
+$events = [new Event ('jep.jpg' , 'event 1' , 'description' , '19 maart 2014' , 'time' , 'website' ,'email'),
+new Event ('apple.jpg' , 'event 2' , 'description' , '21 maart 2014' , 'time' , 'website' ,'email'),
+new Event ('linux.jpg' , 'event 3' , 'description' , '27 maart 2014' , 'time' , 'website', 'email'),
+new Event ('microzach.jpg' , 'event 4' , 'description' , '30 maart 2014' , 'time' , 'website' ,'email'),
+new Event ('twitter1.gif' , 'event 5' , 'description' , '1 april 2014' , 'time' , 'website' ,'email')];
 $content = "";
 foreach ($events as $event) {
 		$content .= new Div(
