87ad2107473f18a6f923b3b361a5c06fc312dd00
Vincent Risack <risackvincent@hotmail.com>
2014-04-25 09:37:59 +0200

    added response page

---
+++ array.php
@@ -0,0 +1,5 @@
+<?php  $events = [new Event ('jep.jpg' , 'event 1' , 'description' , '19 maart 2014' , 'time' , 'website' ,'email'),
+new Event ('apple.jpg' , 'event 2' , 'description' , '21 maart 2014' , 'time' , 'website' ,'email'),
+new Event ('linux.jpg' , 'event 3' , 'description' , '27 maart 2014' , 'time' , 'website', 'email'),
+new Event ('microzach.jpg' , 'event 4' , 'description' , '30 maart 2014' , 'time' , 'website' ,'email'),
+new Event ('twitter1.gif' , 'event 5' , 'description' , '1 april 2014' , 'time' , 'website' ,'email')];
---
+++ response.php
@@ -0,0 +1,31 @@
+<?php 
+include_once("event.php");
+include_once("lib/html.php");
+include_once("array.php");
+$content = "";
+foreach ($events as $event) {
+		$content .= new Div(
+			
+				new Heading($event->title) .
+				new Heading($event->description) .
+				new Heading($event->date) . 
+				new Heading($event->time) .
+				new Heading($event->website) . 
+				new Heading($event-> email)
+				);
+	}
+
+
+
+ ?>
+ <!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php echo $content; ?>
+</body>
+</html>
+
+
