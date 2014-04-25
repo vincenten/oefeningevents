c6209e199cf96423db7291c44a13f009e9545871
Vincent Risack <risackvincent@hotmail.com>
2014-04-25 12:00:20 +0200

    added

--- database.php
+++ database.php
@@ -1,26 +1,14 @@
 <?php
+ìnclude_once
 /* Begin a transaction, turning off autocommit */
 $DBH = new PDO("mysql:host=localhost;dbname=base", "root", "");
 
-
-/* Insert multiple records on an all-or-nothing basis */
 $sql = 'INSERT INTO base
     (id, title, description, date, time, website, email)
     VALUES ($_get[id] + 1, event + id , description, date, time, website, email)';
 
-$sth = $dbh->prepare($sql);
+$sth = $DBH->prepare($sql);
 
-foreach ($events as $event) {
-    $sth->execute(array(
-        $event->title,
-        $event->description,
-        $event->date,
-        $event->time,
- 		$event->website,
- 		$event->email,
-    ));
-}
-
-$dbh->commit();
+$DBH->commit;
 
 ?>
